0d04004ac57adbda159529083e44a867bbe6a15e
sazzad <sazzad.islam@dsinnovators.com>
2018-03-29 18:44:30 +0600

    added route

--- src/main.js
+++ src/main.js
@@ -1,8 +1,26 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App.vue';
+import HelloWorld from './components/HelloWorld'
+import Login from './components/Login'
+import VueRouter from 'vue-router';
+
+Vue.use(VueRouter);
+const routes = [
+    {
+        path: "/",
+        component: HelloWorld
+    },
+    {
+        path: "/login",
+        component: Login
+    }
+];
+
+const router = new VueRouter({routes});
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App)
+    router,
+    render: h => h(App)
 }).$mount('#app')
