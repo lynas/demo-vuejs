1a81d4d8e205575473098d3c27635e9fb717f47b
LynAs Sazzad <szlynas@gmail.com>
2018-03-30 19:42:38 +0600

    axios added for http requestˆ

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -8,16 +8,31 @@
 </template>
 
 <script>
+    import axios from 'axios'
     export default {
         name: 'Login',
+        created: function () {
+            this.sendGet()
+
+        },
         methods: {
             setSession(){
                 this.$session.set("token", "tokenVal")
-            },setSessionNull(){
+            },
+            setSessionNull(){
                 this.$session.set("token", null)
             },
             getSession(){
                 alert(this.$session.get("token"))
+            },
+            sendGet: function () {
+                axios.get("https://api.github.com/users/lynas/repos")
+                    .then(function (response) {
+                        console.log(response.status);
+                        console.log(response.data.length);
+                    }).catch(function (error) {
+                    console.log(error);
+                });
             }
         }
     }
